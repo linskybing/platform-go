8d352bd21d974e4aa50c4ac03d19c047324f49aa
linskybing <linskybing@gmail.com>
2025-07-19 19:08:23 +0800

    update

--- services/configfile_service.go
+++ services/configfile_service.go
@@ -1,40 +1,111 @@
-package repositories
+package services
 
-func CreateConfigFile(cf *models.ConfigFile) error {
-    return db.DB.Create(cf).Error
-}
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
 
-func GetConfigFileByID(id uint) (*models.ConfigFile, error) {
-    var cf models.ConfigFile
-    if err := db.DB.First(&cf, id).Error; err != nil {
-        return nil, err
-    }
-    return &cf, nil
-}
+	"github.com/gin-gonic/gin"
+	"github.com/linskybing/platform-go/dto"
+	"github.com/linskybing/platform-go/models"
+	"github.com/linskybing/platform-go/repositories"
+	"github.com/linskybing/platform-go/utils"
+)
 
-func UpdateConfigFile(cf *models.ConfigFile) error {
-    if cf.CFID == 0 {
-        return errors.New("missing ConfigFile ID")
-    }
-    return db.DB.Save(cf).Error
-}
-
-func DeleteConfigFile(id uint) error {
-    return db.DB.Delete(&models.ConfigFile{}, id).Error
-}
+var (
+	ErrConfigFileNotFound    = errors.New("config file not found")
+	ErrYAMLParsingFailed     = errors.New("YAML parsing failed")
+	ErrNoInvalidYAMLDocument = errors.New("no valid YAML documents found")
+	ErrUploadYAMLFailed      = errors.New("failed to upload YAML file")
+)
 
 func ListConfigFiles() ([]models.ConfigFile, error) {
-    var list []models.ConfigFile
-    if err := db.DB.Find(&list).Error; err != nil {
-        return nil, err
-    }
-    return list, nil
+	return repositories.ListConfigFiles()
 }
 
-func GetConfigFilesByProjectID(projectID uint) ([]models.ConfigFile, error) {
-    var files []models.ConfigFile
-    if err := db.DB.Where("project_id = ?", projectID).Find(&files).Error; err != nil {
-        return nil, err
-    }
-    return files, nil
+func GetConfigFile(id uint) (*models.ConfigFile, error) {
+	return repositories.GetConfigFileByID(id)
+}
+
+func CreateConfigFile(c *gin.Context, cf dto.CreateConfigFileInput) (*models.ConfigFile, error) {
+
+	filename := fmt.Sprintf("config_%d_%d.yaml", cf.ProjectID, time.Now().Unix())
+	if err := utils.UploadObject(c, filename, "application/x-yaml", strings.NewReader(cf.RawYaml), int64(len(cf.RawYaml))); err != nil {
+		return nil, ErrUploadYAMLFailed
+	}
+
+	createdCF := &models.ConfigFile{
+		Filename:  filename,
+		MinIOPath: fmt.Sprintf("config/%s", filename),
+		ProjectID: cf.ProjectID,
+	}
+
+	if err := repositories.CreateConfigFile(createdCF); err != nil {
+		return nil, err
+	}
+
+	// 	userID, _ := utils.GetUserIDFromContext(c)
+	// _ = utils.LogAudit(c, userID, "update", "config_file", existing.CFID, oldCF, *existing, "")
+
+	// yamlArray := utils.SplitYAMLDocuments(cf.RawYaml)
+	// if len(yamlArray) == 0 {
+	// 	return nil, ErrNoInvalidYAMLDocument
+	// }
+
+	// for _, doc := range yamlArray {
+	// 	yamlContent, _ := utils.YAMLToJSON(doc)
+
+	// }
+	return nil, nil
+}
+
+func UpdateConfigFile(c *gin.Context, id uint, input dto.ConfigFileUpdateDTO) (*models.ConfigFile, error) {
+	existing, err := repositories.GetConfigFileByID(id)
+	if err != nil {
+		return nil, ErrConfigFileNotFound
+	}
+
+	oldCF := *existing
+
+	if input.Filename != nil {
+		existing.Filename = *input.Filename
+	}
+	if input.MinIOPath != nil {
+		existing.MinIOPath = *input.MinIOPath
+	}
+	if input.ProjectID != nil {
+		existing.ProjectID = *input.ProjectID
+	}
+
+	err = repositories.UpdateConfigFile(existing)
+	if err != nil {
+		return nil, err
+	}
+
+	userID, _ := utils.GetUserIDFromContext(c)
+	_ = utils.LogAudit(c, userID, "update", "config_file", existing.CFID, oldCF, *existing, "")
+
+	return existing, nil
+}
+
+func DeleteConfigFile(c *gin.Context, id uint) error {
+	cf, err := repositories.GetConfigFileByID(id)
+	if err != nil {
+		return ErrConfigFileNotFound
+	}
+
+	err = repositories.DeleteConfigFile(id)
+	if err != nil {
+		return err
+	}
+
+	userID, _ := utils.GetUserIDFromContext(c)
+	_ = utils.LogAudit(c, userID, "delete", "config_file", cf.CFID, *cf, nil, "")
+
+	return nil
+}
+
+func ListConfigFilesByProjectID(projectID uint) ([]models.ConfigFile, error) {
+	return repositories.GetConfigFilesByProjectID(projectID)
 }
